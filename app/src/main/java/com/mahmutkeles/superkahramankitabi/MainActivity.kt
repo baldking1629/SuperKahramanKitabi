622d537a05ca320a220a03560c71d5266a39b861
Mahmut KELES <mahmutt.keles38@gmail.com>
2022-12-31 14:20:22 +0300

    süper kahraman kitabı başlangıcı yılın son günü

---
+++ app/src/main/java/com/mahmutkeles/superkahramankitabi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mahmutkeles.superkahramankitabi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
